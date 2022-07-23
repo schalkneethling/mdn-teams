20bc19bb636fb1e46bda2cbf3cd911b17ede3001
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-07-23 17:35:11 +0200

    handle default route differently

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ import getMemberDetails from "./data.js";
 
 http
   .createServer(function (req, res) {
-    if (req.url.includes("/")) {
-      res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-      res.write(JSON.stringify({ msg: "Hello World" }));
-      res.end();
-    }
-
     if (req.url.includes("/members")) {
       getMemberDetails().then((response) => {
         res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
         res.write(JSON.stringify(response));
         res.end();
       });
+    } else {
+      res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+      res.write(JSON.stringify({ msg: "Hello World" }));
+      res.end();
     }
   })
   .listen(8080);
